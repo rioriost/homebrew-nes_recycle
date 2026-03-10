62dd9a0234ac70db3503a834975d414b013a896c
Rio Fujita <rio_github@rio.st>
2026-03-10 15:41:12 +0900

    remove garbage from README

--- Formula/nes_recycle.rb
+++ Formula/nes_recycle.rb
@@ -3,8 +3,8 @@ class NesRecycle < Formula
 
   desc "CLI tool for previewing and submitting the Nespresso recycling pickup form over HTTP"
   homepage "https://github.com/rioriost/homebrew-nes_recycle"
-  url "https://files.pythonhosted.org/packages/90/ac/847a0c2f5c67394ed9fe60aa1b75aa0da893be9eafe8fff8f2fa752b6cd5/nes_recycle-0.0.2.tar.gz"
-  sha256 "6c316827f8325ed393997f0c7d36abcf146abb7562f2d1554ed73913ced10c57"
+  url "https://files.pythonhosted.org/packages/61/a9/77b5d59843fb70f1a4193cb6f9d1829449ef7dda3ba1450a0fd193b90559/nes_recycle-0.0.3.tar.gz"
+  sha256 "744e38972bf9d758d6e181ba8444eb817fea49651e63b0bf8342eb4d83d98746"
   license "MIT"
 
   depends_on "python@3.14"
